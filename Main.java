9884fa58cf15dece7e8c2f93701ee32b0df68df8
Lucas Sidotti <lucassidotti@gmail.com>
2025-12-08 01:17:49 -0300

    Agregue metodos

--- Main.java
+++ Main.java
@@ -1,6 +1,47 @@
 import java.util.*;
-
 public class Main {
+  public static void agregarLibro(ArrayList<Libro> biblioteca, Scanner sc){
+    System.out.println("Complete los datos del libro");
+    System.out.println("Ingrese el titulo del libro...");
+    String titulo = sc.nextLine();
+    System.out.println("Ingrese el autor...");
+    String autor = sc.nextLine();
+    System.out.println("Ingrese el año...");
+    int edicion = sc.nextInt();
+    sc.nextLine();
+    biblioteca.add(new Libro(titulo, autor, edicion));
+    System.out.println("Libro agregado a la Biblioteca");
+  }
+  public static void listarLibros(ArrayList<Libro> biblioteca){
+    if (biblioteca.isEmpty()) {
+      System.out.println("No hay libros disponibles");
+    } else {
+        System.out.println("Libros disponibles");
+        for (Libro l : biblioteca) {
+          l.infoLibro();
+        }
+      }
+  }
+  public static void registrarUsuario(ArrayList<Usuario> usuarios, Scanner sc){
+    System.out.println("Complete los datos del usuario");
+    System.out.println("Ingrese el nombre...");
+    String nombre = sc.nextLine();
+    System.out.println("Ingrese nro de DNI...");
+    int dni = sc.nextInt();
+    sc.nextLine();
+    usuarios.add(new Usuario(nombre, dni));
+    System.out.println("Nuevo usuario registrado");
+  }
+  public static void listarUsuario(AbstractList<Usuario> usuarios){
+    if (usuarios.isEmpty()) {
+      System.out.println("No hay usuarios registrados");
+    }else{
+      System.out.println("===Usuarios===");
+      for (Usuario u : usuarios) {
+        u.datosUser();
+      }
+    }
+  }
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     ArrayList<Libro> biblioteca = new ArrayList<>();
@@ -17,41 +58,20 @@ public class Main {
       eleccion = sc.nextInt();
       sc.nextLine();
       if (eleccion == 1) {
-        System.out.println("Complete los datos del libro");
-        System.out.println("Ingrese el titulo del libro...");
-        String titulo = sc.nextLine();
-        System.out.println("Ingrese el autor...");
-        String autor = sc.nextLine();
-        System.out.println("Ingrese el año...");
-        int edicion = sc.nextInt();
-        sc.nextLine();
-        biblioteca.add(new Libro(titulo, autor, edicion));
+        agregarLibro(biblioteca, sc);
       }
       if (eleccion == 2) {
-        System.out.println("Libros disponibles: ");
-        for (Libro l : biblioteca) {
-          l.infoLibro();
-        }
-        System.out.println();
+        listarLibros(biblioteca);
       }
       if (eleccion == 3) {
-        System.out.println("Complete los datos del usuario");
-        System.out.println("Ingrese el nombre...");
-        String nombre = sc.nextLine();
-        System.out.println("Ingrese nro de DNI...");
-        int dni = sc.nextInt();
-        sc.nextLine();
-        usuarios.add(new Usuario(nombre, dni));
+        registrarUsuario(usuarios, sc);
       }
       if (eleccion == 4) {
-        System.out.println("===Usuarios===");
-        for (Usuario u : usuarios) {
-          u.datosUser();
-        }
+        listarUsuario(usuarios);
       }
     } while (eleccion != 5);
     System.out.println("Saliendo...");
-
+    sc.close();
   }
 
 }
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Estuve literalmente 1h buscando un error en la compilacion del codigo y no habia nnguno. Que paso? Me di cuenta cuando ejecuté el programa con NeoVim.
+Estuve literalmente 1h buscando un error en la compilacion del codigo y no habia ninguno. Que paso? Me di cuenta cuando ejecuté el programa con NeoVim.
 Este editor muestra la terminal mas grande y clara. No habia ningun error.
 El problema es que la terminal de Vscode es tan chiquita y mi menu tan largo que se mezclaba todo, parececia que no andaba pero anda todo.
