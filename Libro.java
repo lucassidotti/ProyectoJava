f4fae260c63c65c5290d42e35719e0398bdd39e5
Lucas Sidotti <lucassidotti@gmail.com>
2025-12-11 23:52:01 -0300

    Agregue gets

--- Libro.java
+++ Libro.java
@@ -1,13 +1,27 @@
 public class Libro {
-    private String titulo;
-    private String autor;
-    private int edicion;
-    public Libro(String titulo, String autor, int edicion){
-        this.titulo=titulo;
-        this.autor=autor;
-        this.edicion=edicion;
-    }
-    public void infoLibro(){
-        System.out.println("Titulo: "+titulo+" - "+"Autor: "+autor+" - "+"Año: "+edicion);
-    }
+  private String titulo;
+  private String autor;
+  private int edicion;
+
+  public Libro(String titulo, String autor, int edicion) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.edicion = edicion;
+  }
+
+  public String getTitulo() {
+    return titulo;
+  }
+
+  public String getAutor() {
+    return autor;
+  }
+
+  public int getEdicion() {
+    return edicion;
+  }
+
+  public void infoLibro() {
+    System.out.println("Titulo: " + titulo + " - " + "Autor: " + autor + " - " + "Año: " + edicion);
+  }
 }
